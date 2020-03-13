a56d2ac9b7cdc0afc95655df54322e73a2f8cbb6
Eliot Sanford <eliot587@gmail.com>
2020-03-12 23:42:30 -0500

    update: changed where id to match an existing record

--- eliot/update.sql
+++ eliot/update.sql
@@ -6,8 +6,8 @@ ALTER TABLE player
 ADD nickname varchar(20);
 UPDATE player 
 SET nickname = 'Dak'
-WHERE id = 2;
+WHERE id = 1;
 
-SELECT nickname FROM player WHERE id = 2;
+SELECT nickname FROM player WHERE id = 1;
 
 SELECT * FROM player;
